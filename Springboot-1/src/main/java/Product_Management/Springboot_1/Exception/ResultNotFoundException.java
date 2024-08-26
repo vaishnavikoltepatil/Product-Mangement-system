71dd4a1142bcb3224f9d1c08810c90c1924936f1
vaishnavikoltepatil <118435510+vaishnavikoltepatil@users.noreply.github.com>
2024-08-25 18:51:41 -1200

    ProductMangement

--- Springboot-1/src/main/java/Product_Management/Springboot_1/Exception/ResultNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package Product_Management.Springboot_1.Exception;
-
-public class ResultNotFoundException  extends RuntimeException{
-
-    public ResultNotFoundException(String message) {
-        super(message);
-    }
-}
